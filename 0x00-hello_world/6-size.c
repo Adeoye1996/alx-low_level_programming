b846e073c43a28701629eaaf67c68b8af0ae5597
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2023-08-10 16:16:23 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("Size of a chair: %1 byte(s)\n", sizeof(char));
-	printf("Size of an int: %1 byte(s)\n", sizeof(int));
-	printf("Size of a  long int: %1 byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %1 byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %1 byte(s)\n", sizeof(float));
-	return (0);
+	printf("Size of a chair: %l byte(s)\n", sizeof(char));
+        printf("Size of an int: %l byte(s)\n", sizeof(int));
+        printf("Size of a  long int: %l byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %l byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %l byte(s)\n", sizeof(float));
+        return (0);
 }
 
