8e517d61a3334e76871b475acdd1c284c6c0ccee
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2023-08-17 15:42:49 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - find and print
+ * follow by a new line
+ * Return : 0
+ */
+int main(void)
+{
+	int i;
+	unsigned  long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	prinf("%lu\n", sum)
+		return (0);
+}
