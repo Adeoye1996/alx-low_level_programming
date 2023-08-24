80cb113b48ee6c9384d07fae47dc5c046ed16f81
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2023-08-23 21:22:02 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,9 +12,10 @@
 char *rot13(char *s)
 
 {
-	int i
+	int i;
 
-		int j;
+	int j;
+
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
