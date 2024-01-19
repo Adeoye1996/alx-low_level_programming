9fe508df3f361094c3d6cad10b72862d32722945
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-01-19 21:50:59 +0000

    operations

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* add - Add two integers
+* @a: The first unit
+* @b: The second int
+* Return: Addition
+*/
+int add(int a, int b)
+{
+        return (a + b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* sub - Subtract two integers
+* @a: The first unit
+* @b: The second int
+* Return: Subtracted result
+*/
+int sub(int a, int b)
+{
+        return (a - b);
+}
